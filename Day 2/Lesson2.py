ba7537283ab88f7790107400293483185dbee3a4
nikadola <dolidzenika927@gmail.com>
2023-10-31 18:32:09 +0400

    second commit

--- Day 2/Lesson2.py
+++ Day 2/Lesson2.py
@@ -1,1 +1,21 @@
-print("this is day 2")
+name = "nika"
+surname = "dolidze"
+age = 21
+height = 184
+weight = 70
+
+
+I_am_Nika = True
+
+my_surname_is_Dolidze = True
+
+I_am_22_years_old = True
+
+my_height_is_184_cm = True
+
+my_weight_is_70_kg = True
+
+
+name = "nika"
+
+print(name + " " + surname + " " + str(age) + " " + str(height) + " " + str(weight))
